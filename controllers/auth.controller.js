ae62ef06992d810366a2865663a9a7474a1de847
nvkex <scrbwrm@gmail.com>
2020-10-14 19:49:02 +0530

    log auth history to DB

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,19 +1,20 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const AuthHistory = require('../models/AuthHistory');
 
 exports.loginUser = async (req, res) => {
     const user = await User.findOne({
-        email:req.body.email
+        email: req.body.email
     });
 
-    if(!user){
-        return res.status(400).json({error:'User doesnt exist'});
+    if (!user) {
+        return res.status(400).json({ error: 'User doesnt exist' });
     }
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if(!validPassword){
-        return res.status(400).send({error:'Invalid Password'});
+    if (!validPassword) {
+        return res.status(400).send({ error: 'Invalid Password' });
     }
 
     const token = jwt.sign(
@@ -25,14 +26,32 @@ exports.loginUser = async (req, res) => {
         process.env.JWT_TOKEN
     );
 
-    res.header('auth-token', token).send({token, user});
+    // Log this to Authentication History
+    AuthHistory.updateOne(
+        { date: new Date().toString().slice(4, 15) },
+        {
+            $addToSet: {
+                history: [{
+                    authType: "Login",
+                    userId: user._id,
+                    name: user.name,
+                    isAdmin: user.admin,
+                    date: new Date()
+                }]
+            }
+        },
+        { upsert: true }
+    ).then(data => {})
+    .catch(err => {})
+
+    res.header('auth-token', token).send({ token, user });
 }
 
 exports.signupUser = async (req, res) => {
-    const emailExists = await User.findOne({email:req.body.email});
+    const emailExists = await User.findOne({ email: req.body.email });
 
-    if(emailExists){
-        return res.status(400).json({error: 'Email already exists'})
+    if (emailExists) {
+        return res.status(400).json({ error: 'Email already exists' })
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -44,12 +63,32 @@ exports.signupUser = async (req, res) => {
         password: hashedPassword
     });
 
-    try{
+    try {
         const saveUser = await newUser.save();
-        res.send({status:200, msg: "User Created!"});
+        console.log(saveUser)
+
+        // Log this to Authentication History
+        AuthHistory.updateOne(
+            { date: new Date().toString().slice(4, 15) },
+            {
+                $addToSet: {
+                    history: [{
+                        authType: "SignUp",
+                        userId: saveUser._id,
+                        name: saveUser.name,
+                        isAdmin: saveUser.admin,
+                        date: new Date()
+                    }]
+                }
+            },
+            { upsert: true }
+        ).then(data => {})
+        .catch(err => {})
+
+        res.send({ status: 200, msg: "User Created!" });
     }
-    catch(err){
-        res.send({status: 400, error: err});
+    catch (err) {
+        res.send({ status: 400, error: err });
     }
 }
 
