8e001a73d5aa8aeb6a4332ae3c5970edd2c1fc8c
nvkex <scrbwrm@gmail.com>
2020-10-15 15:08:18 +0530

    feat: Transaction mail service

--- utils/mail.util.js
+++ utils/mail.util.js
@@ -1,5 +1,4 @@
 const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport');
 
 exports.welcomeMail = (userEmail, name) => {
     const transporter = nodemailer.createTransport({
@@ -22,6 +21,31 @@ exports.welcomeMail = (userEmail, name) => {
             console.log(error);
         } else {
             console.log('Welcome mail sent: ' + info.response);
+        }
+    });
+}
+
+exports.transactionMail = (userEmail, name, product) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.COMPANY_EMAIL,
+            pass: process.env.COMPANY_PASSWORD
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.COMPANY_EMAIL,
+        to: userEmail,
+        subject: 'Order Confirmed!',
+        text: `Hello ${name}! Transaction for product:${product} successful!`
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Transaction mail sent: ' + info.response);
         }
     });
 }
